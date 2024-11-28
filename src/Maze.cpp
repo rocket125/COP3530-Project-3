fddfd67eca2ab481a16767bd8c41747e35feb0db
rocket125 <149022535+rocket125@users.noreply.github.com>
2024-11-27 23:47:29 -0500

    Added Maze data structure

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -1,2 +1,78 @@
 #include "Maze.h"
+#include <iostream>
 
+using namespace std;
+
+Maze::Maze()
+{
+    startPos = make_pair(0,0);
+    endPos = make_pair(0,0);
+    maze = vector<string>();
+}
+
+const vector<string>& Maze::getMaze()
+{
+    return maze;
+}
+
+pair<int, int> Maze::getStartPosition()
+{
+    return startPos;
+}
+
+pair<int, int> Maze::getEndPosition()
+{
+    return endPos;
+}
+
+bool Maze::isValidTile(unsigned int x, unsigned int y)
+{
+    return (x < maze[y].length() && y < maze.size());
+}
+
+void Maze::setMaze(const vector<string>& maze)
+{
+    this->maze = maze;
+}
+
+bool Maze::setTile(unsigned int x, unsigned int y, char t)
+{
+    if (isValidTile(x, y))
+    {
+        maze[y][x] = t;
+        return true;
+    }
+    return false;
+}
+
+bool Maze::setStartPosition(unsigned int x, unsigned int y)
+{
+    if (isValidTile(x, y))
+    {
+        startPos = make_pair(x, y);
+        return true;
+    }
+    return false;
+}
+
+bool Maze::setEndPosition(unsigned int x, unsigned int y)
+{
+    if (isValidTile(x, y))
+    {
+        endPos = make_pair(x, y);
+        return true;
+    }
+    return false;
+}
+
+void Maze::printMaze()
+{
+    for (unsigned int y = 0; y < maze.size(); ++y)
+    {
+        for (unsigned int x = 0; x < maze[y].length(); ++x)
+        {
+            cout << maze[y][x];
+        }
+        cout << endl;
+    }
+}
