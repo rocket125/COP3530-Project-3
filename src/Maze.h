e0cb035bb3ebae2dd4550fc69aa16456da4fba8d
rocket125 <149022535+rocket125@users.noreply.github.com>
2024-11-30 17:25:18 -0500

    Added Maze::generateMaze() static method

--- src/Maze.h
+++ src/Maze.h
@@ -3,6 +3,7 @@
 #include <utility>
 #include <vector>
 #include <string>
+#include <set>
 
 using namespace std;
 
@@ -17,18 +18,28 @@ class Maze {
     vector<string> maze;
     pair<int,int> startPos, endPos;
 
-    // Private functions
+    // Private methods
+
+    // Private static methods
+    static void markEdges(pair<unsigned int, unsigned int>& tile, set<pair<unsigned int, unsigned int>>& inSet, set<pair<unsigned int, unsigned int>>& neighbors, Maze m); // Removes tile from neighbor set and places it in inSet. Its neighbors is placed in the neighbors set.
 
 public:
     Maze(); // Constructs a maze of 0 size
     // Accessors
     const vector<string>& getMaze();
+    vector<pair<unsigned int, unsigned int>> getAdjacentTiles(unsigned int x, unsigned int y);
+    vector<pair<unsigned int, unsigned int>> getAdjacentEdges(unsigned int x, unsigned int y);
+    pair<unsigned int, unsigned int> getEdgeBetween(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2); // Returns the position of an edge connecting two tiles.
+    vector<pair<unsigned int, unsigned int>> getTilesFromEdge(unsigned int x, unsigned int y);
+    vector<pair<unsigned int, unsigned int>> getTilesFromEdge(pair<unsigned int, unsigned int> edge);
     pair<int,int> getStartPosition();
     pair<int,int> getEndPosition();
-    bool isValidTile(unsigned int x, unsigned int y); // Returns true if the tile is within the maze's bounds
+    bool isValidTile(int x, int y); // Returns true if the tile is within the maze's bounds
+    bool isValidTile(pair<int, int> tile); // Returns true if the tile is within the maze's bounds
     // Mutators
     void setMaze(const vector<string>& maze); // Replaces maze with a new vector<string> maze
     bool setTile(unsigned int x, unsigned int y, char t); // Replaces a tile within the maze. Returns false if the tile does not exist
+    bool setTile(pair<unsigned int, unsigned int>& position, char t); // Replaces a tile within the maze. Returns false if the tile does not exist
     bool setStartPosition(unsigned int x, unsigned int y);
     bool setEndPosition(unsigned int x, unsigned int y);
     // Misc
@@ -38,7 +49,5 @@ public:
     static void loadMazeFromFile(Maze& maze,const string& path);
     static Maze generateMaze(unsigned int width, unsigned int height); // Generates a random maze.
 };
-
-
 
 #endif //MAZE_H
--- test/test.cpp
+++ test/test.cpp
@@ -7,5 +7,20 @@ TEST_CASE("Loading maze 5x5", "[nogui]")
 {
     Maze maze;
     Maze::loadMazeFromFile(maze, "mazes/5x5.txt");
+    vector<string> actual = maze.getMaze();
+    vector<string> expected =
+    {"##s##",
+"#....",
+"..###",
+"#...#",
+"###t#"};
+    maze.printMaze();
+    REQUIRE(actual == expected);
+}
+
+TEST_CASE("Loading random", "")
+{
+    srand(123123143);
+    Maze maze = Maze::generateMaze(31, 16);
     maze.printMaze();
 }
